bc44bebd1d571a0c1e2075a6199c7bc033368f3b
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2020-04-14 18:06:30 +0200

    Improved look behind reflex.

--- E06-TDMBot/src/main/java/hlaa/tdm/behavior/LookBehindReflex.java
+++ E06-TDMBot/src/main/java/hlaa/tdm/behavior/LookBehindReflex.java
@@ -1,14 +1,14 @@
 package hlaa.tdm.behavior;
 
-import cz.cuni.amis.pogamut.ut2004.bot.impl.UT2004BotModuleController;
+import cz.cuni.amis.pogamut.base3d.worldview.object.Location;
 import cz.cuni.amis.pogamut.ut2004.teamcomm.bot.UT2004BotTCController;
-import hlaa.tdm.utils.DeltaCounter;
+import cz.cuni.amis.utils.Cooldown;
+import java.util.concurrent.TimeUnit;
 
 public class LookBehindReflex extends BaseReflex {
 
-    private static final double LOOK_BEHIND_EVERY_S = 5.0;
-    private final DeltaCounter _deltaCounter = new DeltaCounter();
-    private double _delta = 0;
+    private static final Cooldown _lookBehindCooldown = new Cooldown(15, TimeUnit.SECONDS);
+    private static Location _toLook = null;
 
     public LookBehindReflex(UT2004BotTCController bot) {
         super(bot);
@@ -16,14 +16,19 @@ public class LookBehindReflex extends BaseReflex {
 
     @Override
     public boolean triggered() {
-        _delta += _deltaCounter.getDelta();
-        return _bot.getPlayers().getVisiblePlayers().size() == 0 && _delta > LOOK_BEHIND_EVERY_S;
+        return _bot.getPlayers().getVisiblePlayers().size() == 0 && (_lookBehindCooldown.isCool() || _toLook != null);
     }
 
     @Override
     public void execute() {
-        _bot.getLog().info("Look behind");
-        _bot.getMove().turnHorizontal(180);
-        _delta -= LOOK_BEHIND_EVERY_S;
+        if(_lookBehindCooldown.tryUse()) {
+            _bot.getLog().info("Look behind");
+            _toLook = _bot.getInfo().getLocation().sub(_bot.getInfo().getRotation().toLocation().scale(1000));
+        }
+        _bot.getNavigation().setFocus(_toLook);
+        if(_bot.getInfo().isFacing(_toLook, 35)){
+            _toLook = null;
+            _bot.getNavigation().setFocus(null);
+        }
     }
 }
